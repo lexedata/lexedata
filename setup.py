66f8cc39cc46167e78ab0dd2edaa9e8a51fe6bc3
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-02-13 13:39:51 +0100

    Move modules to package directory and provide setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lexicaldatabase",
+    version="0.1",
+    description="Tools for editing lexical databases for historical linguistics",
+    author="Melvin Steiger, Gereon A. Kaiping",
+    author_email="gereon.kaiping@geo.uzh.ch",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='data linguistics',
+    license='???',
+    url="https://gitlab.uzh.ch/gereonalexander.kaiping/editinglexicaldatasets"
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    platforms='any',
+    install_requires=[
+        "openpyxl",
+        "unidecode"]
+    )
