760c9b81f733e55aafd34f2c73567251b272b5bc
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-02-21 11:57:07 +0100

    For test discovery, need 'test' in filename

--- test/database.py
+++
@@ -1,6 +0,0 @@
-from lexedata.importer.database import create_db_session
-
-def test_create_memory_db():
-    session = create_db_session(location='sqlite:///:memory:')
-    session.connection().engine.dispose()
-
