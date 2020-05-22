4c4a25f421ecd81b1dfa228b733b16ab492791e2
Gereon Kaiping <gereon.kaiping@gmail.com>
2020-05-22 13:29:02 +0200

    Fix the test: Take a test data file, instead of the default excel

--- test/test_fromexcel_runs.py
+++ test/test_fromexcel_runs.py
@@ -8,17 +8,19 @@ from lexedata.importer.fromexcel import ExcelParser
 
 
 @pytest.fixture
-def cldf_wordlist():
+def excel_wordlist():
     return Path(__file__).parent / "data/excel/small.xlsx"
 
 
-def test_fromexcel_runs(cldf_wordlist):
+def test_fromexcel_runs(excel_wordlist):
     # The Intermediate Storage, in a in-memory DB
     session = create_db_session("sqlite:///:memory:")
 
     ExcelParser(
         session,
         output=tempfile.mkdtemp(),
-        lexicon_spreadsheet=cldf_wordlist).parse()
+        lexicon_spreadsheet=excel_wordlist,
+        # FIXME: Build an actual minimal cognateset spreadsheet
+        cognatesets_spreadsheet=excel_wordlist).parse()
     session.commit()
     session.close()
