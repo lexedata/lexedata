6eb39e980d13624b641c15f36c35c80a81cf63f9
Walter Fuchs <melvinsamosn.steiger@uzh.ch>
2020-05-27 16:21:34 +0200

    test functions for errors of cellparsers

--- test/test_cellparser.py
+++ test/test_cellparser.py
@@ -1,65 +1,56 @@
 # -*- coding: utf-8 -*-
 import json
 from pathlib import Path
+import pytest
 from unittest import TestCase, TextTestRunner
 
 import openpyxl as op
 
-from lexedata.importer.cellparser import CellParser
-from lexedata.database.objects import Form, Concept
+from lexedata.importer.cellparser import CellParserLexical, CellParser, CellParserCognate
+from lexedata.importer.exceptions import *
 
 
-class TestCellparser(TestCase):
-    def setUp(self, file="test_cellparser.xlsx"):
-        # set path and check for existing excel test file
-        file = Path.cwd() / file
-        if not file.exists():
-            print("There are no test files provided")
-            exit()
-
-        wb = op.load_workbook(filename=file)
-        sheets = wb.sheetnames
-        wb = wb[sheets[0]]
-        # for collecting different types of cells to be tested
-        self.clean_cells = []  # all good
-        self.tolerated_cells = []  # cellparser will change somethings
-        self._error_cells = []  # error will be
-
-        iter_concept = wb.iter_rows(min_row=3, max_col=6)  # iterates over rows with concepts
-        iter_lan = wb.iter_cols(min_row=1, max_row=2, min_col=7)
-        for row in wb.iter_rows(min_row=3, min_col=7):
-            if row[0].row <= 5:
-                for cell in row:
-                    self.clean_cells.append(cell)
-            if row[0].row == 6:
-                for cell in row:
-                    self.tolerated_cells.append(cell)
-            if row[0].row > 6:
-                for cell in row:
-                    self._error_cells.append(cell)
+def test_cellparser_error():
+    cellparser = CellParser()
+    with pytest.raises(NotImplementedError):
+        cellparser.parse_value("dummy", "A1")
 
 
-def main():
+def test_cellparserlexical_errors():
+    cellparser = CellParserLexical()
+    # white spaces in excel cell
+    with pytest.raises(IgnoreCellError):
+        cellparser.parse_value(" ", "A1")
 
-    test = TestCellparser()
-    test.setUp()
-    empyt_concept = Concept(id="NA", set="NA", english="NA", english_strict="NA", spanish="NA", portuguese="NA",
-                            french="NA", concept_comment="NA")
-    mydump = {}
-    for cell in test.clean_cells:
-        forms = []
-        for f in CellParser(cell):
-            form = Form.create_form(f, "no_lan", cell, empyt_concept)
-            form_dic = vars(form)
-            del form_dic["_sa_instance_state"]
-            forms.append(form_dic)
-        mydump[cell.coordinate] = forms
+    # separator  in transcription
+    with pytest.raises(SeparatorCellError):
+        cellparser.parse_value("<tɨ̈nɨmpɨ̈'ä>[tɨ̃nɨ̃mpɨ̃ã; hɨnampɨʔa]", "A1")
 
-    with (Path.cwd() / "clean_cells.json").open("w", encoding="utf8") as out:
-        json.dump(mydump, out, indent=4, sort_keys=True)
+    # incomplete parsing due to wrong order of variants
+    with pytest.raises(CellParsingError):
+        cellparser.parse_value("/ta/ [ta.'ʔa] ['ta] (cabello púbico){4}", "A1")
+    # incomplete parsing due to wrong order of variants
+    with pytest.raises(CellParsingError):
+        cellparser.parse_value("[dʒi'tɨka] {2} ~ [ʒi'tɨka] {2}", "A1")
+    # incomplete parsing due to missing separator
+    with pytest.raises(CellParsingError):
+        cellparser.parse_value(" /a/ [a.'ʔa] (cabello){4} /aʔa/", "A1")
+    # incomplete parsing due to unrecognized separation pattern: (description), (description)
+    with pytest.raises(CellParsingError):
+        cellparser.parse_value("[iɾũndɨ] (H.F.) (parir), (GIVE BIRTH) [mbohaˈpɨ]", "A1")
+    # incomplete parsing due to too manny sources
+    with pytest.raises(CellParsingError):
+        cellparser.parse_value("[dʒi'tɨka] ~ [ʒi'tɨka] {2} {2}", "A1")
+
+    # comment error due to not matching opening and closing brackets
+    with pytest.raises(FormCellError):
+        cellparser.parse_value("<eniãcũpũ> (good-tasting (sweet honey, hard candy, chocolate candy, water))){2}", "A1")
 
 
+def test_cellparsercognate_errors():
+    cellparser = CellParserCognate()
+    # white spaces in excel cell
+    with pytest.raises(IgnoreCellError):
+        cellparser.parse_value("DUMMY", "A1")
 
 
-if __name__ == "__main__":
-    main()
