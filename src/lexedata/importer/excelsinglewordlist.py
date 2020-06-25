4d8bd8dd9024361ebfb7a7619124c3e8afb5a3d8
Gereon Kaiping <gereon.kaiping@gmail.com>
2020-06-25 16:02:10 +0200

    Add some safeguards

--- src/lexedata/importer/excelsinglewordlist.py
+++ src/lexedata/importer/excelsinglewordlist.py
@@ -9,26 +9,34 @@ def import_language(
         writer: csv.DictWriter) -> None:
     language = sheet.title
     fn: t.Iterable[str]
+    empty_cols: t.Set[int] = set()
+    for i, col in enumerate(sheet.iter_cols()):
+        column = [str(c.value).strip() if c.value is not None else None for c in col]
+        if not any(column[1:]):
+            empty_cols.add(i)
+
     for i, row in enumerate(sheet.iter_rows()):
         if i == 0:
             header = [
                 n.value or f'c{c:}'
-                for c, n in enumerate(row)]
+                for c, n in enumerate(row)
+                if c not in empty_cols]
             diff1 = (set(writer.fieldnames) - {"Language"}) - set(header)
             diff2 = set(header) - (set(writer.fieldnames) - {"Language"})
             if diff1 or diff2:
                 logging.warning(
-                    "Headers mismatch in sheet %s: %s vs. %s",
+                    "Headers mismatch in sheet %s: expected %s but found %s",
                     language,
                     diff1,
                     diff2)
         data = {
-            k: (int(r.value)
-                if int(r.value) == r.value
-                else r.value)
-            if type(r.value) == float
-            else r.value
-            for k, r in zip(header, row)
+            k: (int(cell.value)
+                if int(cell.value) == cell.value
+                else cell.value)
+            if type(cell.value) == float
+            else cell.value
+            for k, (c, cell) in zip(header, enumerate(row))
+            if c not in empty_cols
             if k in writer.fieldnames}
         data["Language"] = language
         writer.writerow(data)
@@ -55,8 +63,14 @@ if __name__ == '__main__':
                       if sheet not in args.exclude_sheet]
         logging.warn("No sheets specified. Parsing sheets: %s", args.sheet)
     first_sheet = args.excel[args.sheet[0]]
+    empty_cols: t.Set[int] = set()
+    for i, col in enumerate(first_sheet.iter_cols()):
+        column = [str(c.value).strip() if c.value is not None else None for c in col]
+        if not any(column[1:]):
+            empty_cols.add(i)
     header = [n.value or f'c{c:}'
-              for c, n in enumerate(next(first_sheet.iter_rows(1, 1)))]
+              for c, n in enumerate(next(first_sheet.iter_rows(1, 1)))
+              if c not in empty_cols]
     header.insert(0, "Language")
     d = csv.DictWriter(
         args.csv,
