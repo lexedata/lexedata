9e011467e8abc2a50c673039b896c94ea4f734ff
Walter Fuchs <melvinsamosn.steiger@uzh.ch>
2020-07-14 00:44:29 +0200

    test_cellparser runs with new cellparser

--- test/test_cellparser.py
+++ test/test_cellparser.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
-import json
-from pathlib import Path
 import pytest
-from unittest import TestCase, TextTestRunner
 
-import openpyxl as op
-
-from lexedata.importer.cellparser2 import AbstractCellParser, TupiFormCellParser#, CellParserCognate
-from lexedata.importer.cellparser import CellParser, CellParserLexical, CellParserCognate
+from lexedata.importer.cellparser import AbstractCellParser, MawetiGuaraniLexicalParser, MawetiGuaraniCognateParser
 from lexedata.importer.exceptions import *
 
 
 def test_cellparser_error():
-    cellparser = CellParser()
+    cellparser = AbstractCellParser()
     with pytest.raises(NotImplementedError):
         cellparser.parse_value("dummy", "A1")
 
 
 def test_cellparserlexical_errors():
-    cellparser = CellParserLexical()
+    cellparser = MawetiGuaraniLexicalParser()
     # white spaces in excel cell
     with pytest.raises(IgnoreCellError):
         cellparser.parse_value(" ", "A1")
@@ -47,13 +41,6 @@ def test_cellparserlexical_errors():
     with pytest.raises(FormCellError):
         cellparser.parse_value("<eniãcũpũ> (good-tasting (sweet honey, hard candy, chocolate candy, water))){2}",
                                "A1")
-
-
-def more():
-    for cell in []:
-        for f in CellParser(cell):
-            pass
-
     # incomplete parsing due to wrong order of variants
     with pytest.raises(CellParsingError):
         cellparser.parse_value("/ta/ [ta.'ʔa] ['ta] (cabello púbico){4}", "A1")
@@ -70,28 +57,9 @@ def more():
     with pytest.raises(CellParsingError):
         cellparser.parse_value("[dʒi'tɨka] ~ [ʒi'tɨka] {2} {2}", "A1")
 
-    # comment error due to not matching opening and closing brackets
-    with pytest.raises(FormCellError):
-        cellparser.parse_value("<eniãcũpũ> (good-tasting (sweet honey, hard candy, chocolate candy, water))){2}", "A1")
-
-    c1 = Tester(
-        "<tatatĩ>(humo){Guasch1962:717}, <timbo>(vapor, vaho, humareda, humo){Guasch1962:729};<tĩ> (humo, vapor de agua)$LDM:deleted 'nariz, pico, hocico, punta, and ápica' meanings; source incorrectly merges 'point' and 'smoke' meanings ${Guasch1962:729}")
-    c2 = Tester(
-        "/pãlĩ/ (de froment = wheat) (NCP: loan from french farine), /pɨlatɨ/ (de mais cru), /kuʔi/ (de mais grillé)")
-    c3 = Tester(
-        "/pãlĩ/ (de froment = wheat(NCP: loan from french farine), &<dummy>), /pɨlatɨ/ (de mais cru), /kuʔi/ (de mais grillé)")
-    c4 = Tester("/popɨãpat/ 'back of elbow' {4}")
-    c5 = Tester("<ayu> (nominalized &/afaaa/ version of &/ete/ 'about') {2}")
-    for ele in [c1, c2, c3, c4, c5]:
-        print(ele.value)
-        print("is represented as: ")
-        for f in CogCellParser(ele):
-            print(f)
-        input()
-
 
 def test_cellparsercognate_errors():
-    cellparser = CellParserCognate()
+    cellparser = MawetiGuaraniCognateParser()
     # white spaces in excel cell
     with pytest.raises(IgnoreCellError):
         cellparser.parse_value("DUMMY", "A1")
