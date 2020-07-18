2875f465c378e4f717163355c1d0e546d1ad44d7
Walter Fuchs <melvinsamosn.steiger@uzh.ch>
2020-07-18 02:32:26 +0200

    working on roundtrip test...

--- test/test_excel_conversion.py
+++ test/test_excel_conversion.py
@@ -17,10 +17,10 @@ def excel_wordlist():
 
 
 @pytest.fixture(params=[
-        "data/cldf/minimal/cldf-metadata.json",
-        "data/cldf/smallmawetiguarani/cldf-metadata.json"])
+        ("data/cldf/minimal/cldf-metadata.json", "data/cldf/minimal/db.sq"),
+        ("data/cldf/smallmawetiguarani/cldf-metadata.json", "data/cldf/smallmawetiguarani/db.sq")])
 def cldf_wordlist(request):
-    return Path(__file__).parent / request.param
+    return Path(__file__).parent / request.param[0], Path(__file__).parent / request.param[1]
 
 
 @pytest.fixture
@@ -40,7 +40,7 @@ def empty_cldf_wordlist():
 def filled_cldf_wordlist(cldf_wordlist):
     # Copy the dataset to a different temporary location, so that editing the
     # dataset will
-    original = cldf_wordlist
+    original = cldf_wordlist[0]
     dirname = Path(tempfile.mkdtemp(prefix="lexedata-test"))
     target = dirname / original.name
     shutil.copyfile(original, target)
@@ -55,7 +55,7 @@ def filled_cldf_wordlist(cldf_wordlist):
     t = target.parent / link
     shutil.copyfile(o, t)
     dataset.sources = pycldf.dataset.Sources.from_file(dataset.bibpath)
-    return dataset
+    return dataset, cldf_wordlist[1]
 
 
 def test_fromexcel_runs(excel_wordlist, empty_cldf_wordlist):
@@ -68,28 +68,30 @@ def test_fromexcel_runs(excel_wordlist, empty_cldf_wordlist):
 
 
 def test_toexcel_runs(filled_cldf_wordlist):
-    writer = ExcelWriter(filled_cldf_wordlist)
+    writer = ExcelWriter(filled_cldf_wordlist[0], filled_cldf_wordlist[1])
     _, out_filename = tempfile.mkstemp(".xlsx", "cognates")
     writer.create_excel(out_filename)
 
 
 def test_roundtrip(filled_cldf_wordlist):
-    c_formReference = filled_cldf_wordlist["CognateTable", "formReference"].name
-    c_cogsetReference = filled_cldf_wordlist["CognateTable", "cognatesetReference"].name
+    c_formReference = filled_cldf_wordlist[0]["CognateTable", "formReference"].name
+    c_cogsetReference = filled_cldf_wordlist[0]["CognateTable", "cognatesetReference"].name
     old_judgements = {
         (row[c_formReference], row[c_cogsetReference])
-        for row in filled_cldf_wordlist["CognateTable"].iterdicts()}
-    writer = ExcelWriter(filled_cldf_wordlist)
+        for row in filled_cldf_wordlist[0]["CognateTable"].iterdicts()}
+    writer = ExcelWriter(filled_cldf_wordlist[0], filled_cldf_wordlist[1])
     _, out_filename = tempfile.mkstemp(".xlsx", "cognates")
+    out_filename = r"C:\Users\walter.fuchs\Desktop\outofasia\tupiguarani-data\test\data\excel\dump.xlsx"
     writer.create_excel(out_filename)
 
     # Reset the existing cognatesets and cognate judgements, to avoid
     # interference with the the data in the Excel file
-    filled_cldf_wordlist["CognateTable"].write([])
+    filled_cldf_wordlist[0]["CognateTable"].write([])
     # filled_cldf_wordlist["CognatesetTable"].write([])
 
     # ExcelCognateParser(cldf.Dataset.from_metadata(...))
-    parser = ExcelCognateParser(filled_cldf_wordlist, excel_file=out_filename)
+
+    parser = ExcelCognateParser(filled_cldf_wordlist[0], excel_file=out_filename)
     parser.left = len(writer.header) + 1
     parser.parse_cells()
     # Really? Isn't there a shortcut to do this?
