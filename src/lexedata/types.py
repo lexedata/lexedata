47cce3c98e59f92550eb5fd04141c2b812815b1c
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2020-08-21 15:30:44 +0200

    moved fnct string_to_id to lexedata\util.py

--- src/lexedata/types.py
+++ src/lexedata/types.py
@@ -62,32 +62,3 @@ class Judgement(RowObject):
     def __table__(self) -> str:
         return "CognateTable"
 
-
-# TODO: This function is a bit out of place here. Move it somewhere more
-# sensible.
-import re
-import unidecode as uni
-invalid_id_elements = re.compile(r"\W+")
-
-
-def string_to_id(string: str) -> str:
-    """Generate a useful id string from the string
-
-    >>> string_to_id("trivial")
-    'trivial'
-    >>> string_to_id("Just 4 non-alphanumerical characters.")
-    'just_4_non_alphanumerical_characters'
-    >>> string_to_id("Это русский.")
-    'eto_russkii'
-    >>> string_to_id("该语言有一个音节。")
-    'gai_yu_yan_you_yi_ge_yin_jie'
-    >>> string_to_id("この言語には音節があります。")
-    'konoyan_yu_nihayin_jie_gaarimasu'
-
-    """
-    # We nee to run this through valid_id_elements twice, because some word
-    # characters (eg. Chinese) are unidecoded to contain non-word characters.
-    return invalid_id_elements.sub(
-        "_", uni.unidecode(
-            invalid_id_elements.sub("_", string)).lower()).strip("_")
-
