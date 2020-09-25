8f3d68c99478e1938b63d8f518a104831bc2d6a7
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2020-09-25 16:16:06 +0200

    debugging doctests of cellparser

--- src/lexedata/importer/cellparser.py
+++ src/lexedata/importer/cellparser.py
@@ -397,11 +397,11 @@ class MawetiCellParser(CellParser):
             properties: t.Dict[str, t.Any],
             language_id: str) -> None:
         """
-        >>> m = MawetiCellParser()
+        >>> m = MawetiCellParser(dict(), dict(), "", [], "{1}")
         >>> form = {
         ...  "orthographic": "<lexedata % lexidata>",
-        ...  "phonemic": "/lεksedata ~ lεksidata/"
-        ...  "variants": ["(from lexicon + edit + data)", "(another comment)"]
+        ...  "phonemic": "/lεksedata ~ lεksidata/",
+        ...  "variants": ["(from lexicon + edit + data)", "(another comment)"],
         ...  "cldf_comment": "(GAK: We should pick one of those names, I'm 80% sure it should be the first)"
         ... }
         >>> m.postprocess_form(form, "abui1241")
@@ -409,12 +409,10 @@ class MawetiCellParser(CellParser):
         ...  "phonemic": "lεksedata",
         ...  "variants": ["~ <lexidata>", "~ /lεksidata/"],
         ...  "cldf_comment": "from lexicon + edit + data\\tanother comment",
-        ...  "cldf_source": {("abui1241_s1", None)}
+        ...  "cldf_source": {("abui1241_s1", None)},
         ...  "procedural_comment": "GAK: We should pick one of those names, I'm 80% sure it should be the first"}
         True
         """
-        # post processing of base class
-        super().postprocess_form(properties, language_id)
         # catch procedural comments (e.g. NPC: ...) in cldf_comments and add to corresponding field
         # but a procedural comment could land in variants, thus this case needs to be checked as well
         try:
@@ -488,6 +486,9 @@ class MawetiCellParser(CellParser):
                         properties[key] = value.pop(0)
                         for v in value:
                             variants.append((separator + v))
+
+        # post processing of base class
+        super().postprocess_form(properties, language_id)
 
 
 class MawetiCognateCellParser(MawetiCellParser):
