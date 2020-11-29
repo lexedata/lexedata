be49ed2ccfdfb62e5c4f67d7bf3d1afe52cb571c
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2020-11-30 00:19:04 +0100

    parametrized tests over excel_list (minimal and maweti)

--- test/test_excel_conversion.py
+++ test/test_excel_conversion.py
@@ -38,13 +38,18 @@ def empty_copy_of_cldf_wordlist(cldf_wordlist):
     return dataset, original
 
 
-@pytest.fixture
-def excel_wordlist():
+@pytest.fixture(
+    params=[
+        ("data/excel/small.xlsx", "data/excel/small_cog.xlsx", "data/cldf/smallmawetiguarani/cldf-metadata.json"),
+        ("data/excel/minimal.xlsx", "data/excel/minimal_cog.xlsx", "data/cldf/minimal/cldf-metadata.json")
+    ]
+)
+def excel_wordlist(request):
     return (
-        Path(__file__).parent / "data/excel/small.xlsx",
-        Path(__file__).parent / "data/excel/small_cog.xlsx",
+        Path(__file__).parent / request.param[0],
+        Path(__file__).parent / request.param[1],
         empty_copy_of_cldf_wordlist(
-            Path(__file__).parent / "data/cldf/smallmawetiguarani/cldf-metadata.json"
+            Path(__file__).parent / request.param[2]
         ),
     )
 
@@ -238,11 +243,11 @@ def test_cell_comments():
     excel_parser_cognate.parse_cells(ws_test)
 
     assert excel_parser_cognate.db.cache["CognateTable"] == {
-        "aaa_1209-cogset": {
-            "ID": "aaa_1209-cogset",
-            "Form_ID": "aaa_1209",
-            "Cognateset": "cogset",
-            "Comment": "Comment on judgement",
+        'autaa_Woman-cogset': {
+            'Cognateset': 'cogset',
+            'Comment': 'Comment on judgement',
+            'Form_ID': 'autaa_Woman',
+            'ID': 'autaa_Woman-cogset'
         }
     }
     assert excel_parser_cognate.db.cache["CognatesetTable"] == {
