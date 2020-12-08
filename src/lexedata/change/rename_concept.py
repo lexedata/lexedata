2b752c742b693378a4067c9c3993b220e49bb8ed
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2020-12-08 14:43:03 +0100

    Add functionality to switch the entire ID column, with a failsafe

--- src/lexedata/change/rename_concept.py
+++ src/lexedata/change/rename_concept.py
@@ -13,14 +13,19 @@ def substitute(row, columns, old_value, new_value):
     return row
 
 
-def rename(ds, original, replacement):
-    concepts = ds["ParameterTable"]
-    c_id = ds["ParameterTable", "id"].name
+def substitute_many(row, columns, old_values_to_new_values):
+    for column in columns:
+        if type(row[column]) == list:
+            row[column] = [
+                old_values_to_new_values.get(val, val) for val in row[column]
+            ]
+        elif type(row[column]) == str:
+            row[column] = old_values_to_new_values.get(row[column], row[column])
+    return row
 
-    print(f"Changing {c_id:} of ParameterTable…")
-    ds.write(
-        ParameterTable=[substitute(r, [c_id], original, replacement) for r in concepts]
-    )
+
+def rename(ds, old_values_to_new_values):
+    concepts = ds["ParameterTable"]
 
     for table in ds.tables:
         if table == concepts:
@@ -40,7 +45,8 @@ def rename(ds, original, replacement):
             ds.write(
                 **{
                     component: [
-                        substitute(r, columns, original, replacement) for r in table
+                        substitute_many(r, columns, old_values_to_new_values)
+                        for r in table
                     ]
                 }
             )
@@ -53,7 +59,40 @@ if __name__ == "__main__":
     parser.add_argument("--metadata", default="Wordlist-metadata.json")
     parser.add_argument("original")
     parser.add_argument("replacement")
+    parser.add_argument("--column-replace", action="store_true", default=False)
+    parser.add_argument("--smush", action="store_true", default=False)
     args = parser.parse_args()
 
     dataset = pycldf.Wordlist.from_metadata(args.metadata)
-    rename(dataset, args.original, args.replacement)
+    if args.column_replace:
+        concepts = dataset["ParameterTable"]
+
+        assert (
+            args.original == "id"
+            or args.original == dataset["ParameterTable", "id"].name
+        ), f"Replacing an entire column is only meaningful when you change the #id column ({dataset['ParameterTable', 'id'].name}) of the ConceptTable."
+
+        c_id = dataset["ParameterTable", args.original].name
+        c_new = dataset["ParameterTable", args.replacement].name
+        mapping = {
+            concept[c_id]: concept[c_new] for concept in dataset["ParameterTable"]
+        }
+        assert args.smush or len(mapping) == len(
+            set(mapping.values())
+        ), "Would collapse some concepts that were distinct before! Add '--smush' if that is intended."
+        breakpoint()
+        dataset["ParameterTable"].tableSchema.columns["c_id"]
+        rename(dataset, {args.original: args.replacement})
+    else:
+        concepts = dataset["ParameterTable"]
+
+        c_id = dataset["ParameterTable", "id"].name
+
+        print(f"Changing {c_id:} of ParameterTable…")
+        dataset.write(
+            ParameterTable=[
+                substitute_many(r, [c_id], {args.original: args.replacement})
+                for r in concepts
+            ]
+        )
+        rename(dataset, {args.original: args.replacement})
