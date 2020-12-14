06d44a67b9752d5e24182b82d59bf668930b47ce
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2020-12-14 15:39:05 +0100

    test for single excel maweti import running

--- test/test_additional_importer.py
+++ test/test_additional_importer.py
@@ -41,18 +41,20 @@ def test_add_forms(cldf_wordlist, concept_name):
     excel = openpyxl.load_workbook(
         Path(__file__).parent / "data/excel/test_single_excel_maweti.xlsx"
     )
-
-    cid = dataset["ParameterTable", "id"].name
-    concepts = {c[concept_name]: c[cid] for c in dataset["ParameterTable"]}
-    concept_column = concept_name
-
+    c_f_id = dataset["FormTable", "id"].name
+    c_c_id = dataset["ParameterTable", "id"].name
+    c_c_name = dataset["ParameterTable", "name"].name
+    concepts = {c[c_c_name]: c[c_c_id] for c in dataset["ParameterTable"]}
+    print(concepts.items())
+    old_form_ids = {row[c_f_id] for row in dataset["FormTable"]}
     sheet = [sheet for sheet in excel.sheetnames]
     for sheet in sheet:
         read_single_excel_sheet(
             dataset=dataset,
             sheet=excel[sheet],
             entries_to_concepts=concepts,
-            concept_column=concept_column,
+            concept_column=concept_name,
         )
-
-    # TODO: Compare results and expected
+    new_form_ids = {row[c_f_id] for row in dataset["FormTable"]}
+    print(new_form_ids)
+    assert new_form_ids - old_form_ids == {"ache_one_1", "ache_one_2"}
