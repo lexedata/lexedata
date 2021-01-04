61168aed19239dfa93e7799c538c178380fb86e2
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-01-04 15:15:05 +0100

    cleaned up command line arguments of exporter/cognates.py

--- src/lexedata/exporter/cognates.py
+++ src/lexedata/exporter/cognates.py
@@ -353,39 +353,44 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--metadata",
-        help="Path to metadata file for dataset input",
+        nargs="?",
+        type=Path,
         default="Wordlist-metadata.json",
+        help="Path to the metadata.json. The metadata file describes the dataset. Default: ./Wordlist-metadata.json",
     )
     parser.add_argument(
-        "--excel", help="Excel output file path", default="Cognates.xlsx"
+        "--excel",
+        help="Excel output file path. Default: ./Cognates.xlsx",
+        default="Cognates.xlsx"
     )
     parser.add_argument(
         "--size-sort",
         action="store_true",
         default=False,
-        help="List the biggest cognatesets first",
+        help="Activate to list the biggest cognatesets first",
     )
     parser.add_argument(
-        "--language-sort-column", help="A column name to sort languages by"
+        "--language-sort-column",
+        help="A column name to sort languages by"
     )
     parser.add_argument(
         "--url-template",
         help="A template string for URLs pointing to individual forms. For example, to"
-        " point to lexibank, you would use https://lexibank.clld.org/values/{:}."
-        " (default: https://example.org/lexicon/{:})",
+             " point to lexibank, you would use https://lexibank.clld.org/values/{:}."
+             " (default: https://example.org/lexicon/{:})",
     )
     parser.add_argument(
         "--add-concepts",
         action="store_true",
         default=False,
-        help="Activate to output the centrale concept associated with each cognateset",
+        help="Activate to output the central concept associated with each cognateset",
     )
     parser.add_argument(
         "--add-singletons",
         action="store_true",
         default=False,
         help="Activate to output all forms that don't belong to a cognateset. "
-        "For each form, a singleton cognateset is created.",
+             "For each form, a singleton cognateset is created.",
     )
     # TODO: Derive URL template from the "special:domain" property of the
     # wordlist, where it exists? So something like
