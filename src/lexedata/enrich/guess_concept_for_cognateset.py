8621fae5723edaa2cc232a36b85e1318f93c8f5b
Gereon Kaiping <gereon.kaiping@gmail.com>
2021-01-05 01:06:51 +0100

    Rewrite cognateset concept guesser

--- src/lexedata/enrich/guess_concept_for_cognateset.py
+++ src/lexedata/enrich/guess_concept_for_cognateset.py
@@ -5,76 +5,65 @@ import pycldf
 import networkx
 from lexedata.util import load_clics
 
+FormID = str
+ConceptID = str
+CognatesetID = str
+
 
 def tqdm(iter, total=0):
     return iter
 
 
-def load_concepts_by_form(dataset: pycldf.Dataset) -> t.Dict[str, str]:
-    try:
-        clics = load_clics()
-    except FileNotFoundError:
-        clics = None
-
-    if clics and dataset.column_names.parameters.concepticonReference:
-        concept_to_concepticon = {
-            row[dataset.column_names.parameters.id]: row[
-                dataset.column_names.parameters.concepticonReference
-            ]
-            for row in dataset["ParameterTable"]
-        }
-        concepticon_to_concept = {
-            row[dataset.column_names.parameters.concepticonReference]: row[
-                dataset.column_names.parameters.id
-            ]
-            for row in dataset["ParameterTable"]
-        }
-        concepticon_concepts_by_form_id: t.Dict[
-            t.Hashable, t.List[t.Optional[t.Hashable]]
-        ] = {}
-        multi = bool(
-            dataset["FormTable"]
-            .get_column(dataset.column_names.forms.parameterReference)
-            .separator
-        )
-        for form in tqdm(
-            dataset["FormTable"],
-            total=dataset["FormTable"].common_props.get("dc:extent"),
-        ):
-            if multi:
-                concepticon_concepts_by_form_id[form[dataset.column_names.forms.id]] = [
-                    concept_to_concepticon.get(c)
-                    for c in form[dataset.column_names.forms.parameterReference]
-                ]
-            else:
-                concepticon_concepts_by_form_id[form[dataset.column_names.forms.id]] = [
-                    concept_to_concepticon.get(
-                        form[dataset.column_names.forms.parameterReference]
-                    )
-                ]
-        central_concepts = {}
-        for form_id, concepts in tqdm(concepticon_concepts_by_form_id.items()):
-            centrality = networkx.algorithms.centrality.betweenness_centrality(
-                clics.subgraph([c for c in concepts if c])
-            )
-            central_concepts[form_id] = concepticon_to_concept.get(
-                max(centrality or concepts, key=lambda k: centrality.get(k, -1))
-            )
-        return central_concepts
-    # if no concepticon references are given or clics is not available
-    central_concepts_by_form_id = dict()
+def load_concepts_by_form(
+    dataset: pycldf.Dataset,
+) -> t.Dict[FormID, t.Sequence[ConceptID]]:
+    """Look up all concepts for each form, and return them as dictionary."""
+    concepts_by_form_id = dict()
     c_f_id = dataset.column_names.forms.id
     c_f_concept = dataset.column_names.forms.parameterReference
-    for form in dataset["FormTable"]:
-        concept = form.get(c_f_concept, "")
-        central_concepts_by_form_id[form[c_f_id]] = (
-            concept[0] if isinstance(concept, list) == str else concept
+    for form in tqdm(
+        dataset["FormTable"],
+        total=dataset["FormTable"].common_props.get("dc:extent"),
+    ):
+        concept = form.get(c_f_concept, [])
+        concepts_by_form_id[form[c_f_id]] = (
+            concept if isinstance(concept, list) == str else [concept]
         )
-    return central_concepts_by_form_id
+    return concepts_by_form_id
 
 
-def reshape_dataset(dataset: pycldf.Dataset, add_column: bool = True) -> pycldf.Dataset:
+def concepts_to_concepticon(dataset: pycldf.Wordlist) -> t.Mapping[ConceptID, int]:
+    concept_to_concepticon = {
+        row[dataset.column_names.parameters.id]: row.get(
+            dataset.column_names.parameters.concepticonReference
+        )
+        for row in tqdm(
+            dataset["ParameterTable"],
+            total=dataset["ParameterTable"].common_props.get("dc:extent"),
+        )
+    }
+    return concept_to_concepticon
 
+
+def central_concept(
+    concepts: t.Counter[ConceptID], concepts_to_concepticon: t.Mapping[ConceptID, int]
+):
+    central_concepts = {}
+    centralities = networkx.algorithms.centrality.betweenness_centrality(
+        clics.subgraph({concepts_to_concepticon.get(c) for c in concepts} - {None})
+    )
+
+    def effective_centrality(cc):
+        concept, count = cc
+        return count * centralities.get(concepts_to_concepticon.get(concept), 1)
+
+    concept, count = max(concepts.items(), key=effective_centrality)
+    return concept
+
+
+def reshape_dataset(
+    dataset: pycldf.Wordlist, add_column: bool = True
+) -> pycldf.Dataset:
     # check for existing cognateset table
     if dataset.column_names.cognatesets is None:
         # Create a Cognateset Table
@@ -96,11 +85,45 @@ def reshape_dataset(dataset: pycldf.Dataset, add_column: bool = True) -> pycldf.
 
 
 def add_central_concepts_to_cognateset_table(
-    dataset: pycldf.Dataset, add_column: bool = True, overwrite_existing: bool = True
+    dataset: pycldf.Dataset,
+    central_concept: t.Mapping[CognatesetID, ConceptID],
+    add_column: bool = True,
+    overwrite_existing: bool = True,
 ) -> pycldf.Dataset:
-    central_concept_by_form_id = load_concepts_by_form(dataset)
-    breakpoint()
     dataset = reshape_dataset(dataset, add_column=add_column)
+    c_core_concept = dataset.column_names.cognatesets.parameterReference
+    if c_core_concept is None:
+        raise ValueError(
+            f"Dataset {dataset:} had no parameterReference column in a CognatesetTable"
+            " and is thus not compatible with this script."
+        )
+
+    # write cognatesets with central concepts
+    write_back = []
+    for row in tqdm(
+        dataset["CognatesetTable"],
+        total=dataset["CognatesetTable"].common_props.get("dc:extent"),
+    ):
+        row[c_core_concept] = central_concept.get(
+            row[dataset.column_names.cognatesets.id]
+        )
+        write_back.append(row)
+    dataset.write(CognatesetTable=write_back)
+    return dataset
+
+
+def connected_concepts(
+    dataset: pycldf.Wordlist,
+) -> t.Mapping[CognatesetID, t.Counter[ConceptID]]:
+    """For each cognate set it the data set, check which concepts it is connected to.
+
+    >>>
+    """
+    concepts_by_form = load_concepts_by_form(dataset)
+    cognatesets_to_concepts: t.DefaultDict[
+        CognatesetID, t.Sequence[ConceptID]
+    ] = t.DefaultDict(list)
+
     # Check whether cognate judgements live in the FormTable …
     c_cognateset = dataset.column_names.forms.cognatesetReference
     c_form = dataset.column_names.forms.id
@@ -116,47 +139,18 @@ def add_central_concepts_to_cognateset_table(
             f"Dataset {dataset:} had no cognatesetReference column in a CognateTable"
             " or a FormTable and is thus not compatible with this script."
         )
-    c_core_concept = dataset.column_names.cognatesets.parameterReference
-    if c_core_concept is None:
-        raise ValueError(
-            f"Dataset {dataset:} had no parameterReference column in a CognatesetTable"
-            " and is thus not compatible with this script."
+
+    for judgement in tqdm(
+        table,
+        total=table.common_props.get("dc:extent"),
+    ):
+        cognatesets_to_concepts[judgement[c_cognateset]].extend(
+            concepts_by_form[judgement[c_form]]
         )
-    # check all cognatesets in table
-    write_back = []
-    cognatesets = set()
-    for row in tqdm(dataset["CognatesetTable"]):
-        write_back.append(row)
-        cognatesets.add(row[dataset.column_names.cognatesets.id])
-    # Load judgements, making sure all cognatesets exist
-    for row in tqdm(table):
-        if row[c_cognateset] not in cognatesets:
-            write_back.append({dataset.column_names.cognatesets.id: row[c_cognateset]})
-    dataset.write(CognatesetTable=write_back)
-    cognatesets = set()
-    for row in tqdm(dataset["CognatesetTable"]):
-        cognatesets.add(row[dataset.column_names.cognatesets.id])
-    # central concepts by cogset id
-    concepts_by_cogset: t.Dict[t.Hashable, t.Hashable] = dict()
-    for row in tqdm(table):
-        cognateset = row[c_cognateset]
-        form = row[c_form]
-        concepts_by_cogset[cognateset] = central_concept_by_form_id[form]
-    # write cognatesets with central concepts
-    write_back = []
-    for row in tqdm(dataset["CognatesetTable"]):
-        if row[dataset.column_names.cognatesets.id] not in concepts_by_cogset:
-            write_back.append(row)
-            continue
-        if row[c_core_concept] and not overwrite_existing:
-            write_back.append(row)
-            continue
-        row[c_core_concept] = concepts_by_cogset[
-            row[dataset.column_names.cognatesets.id]
-        ]
-        write_back.append(row)
-    dataset.write(CognatesetTable=write_back)
-    return dataset
+    return {
+        cogset: t.Counter(concepts)
+        for cogset, concepts in cognatesets_to_concepts.items()
+    }
 
 
 if __name__ == "__main__":
@@ -183,8 +177,28 @@ if __name__ == "__main__":
         help="Activate to overwrite existing Core_Concept_ID of cognatesets",
     )
     args = parser.parse_args()
+    try:
+        clics: t.Optional[networkx.Graph] = load_clics()
+    except FileNotFoundError:
+        clics = None
 
     dataset = pycldf.Wordlist.from_metadata(args.wordlist)
-    dataset = add_central_concepts_to_cognateset_table(
-        dataset, add_column=args.add_column, overwrite_existing=args.overwrite_existing
+
+    concepts_of_cognateset: t.Mapping[
+        CognatesetID, t.Counter[ConceptID]
+    ] = connected_concepts(dataset)
+    central: t.Mapping[str, str] = {}
+    if clics and dataset.column_names.parameters.concepticonReference:
+        concept_to_concepticon = concepts_to_concepticon(dataset)
+        for cognateset, concepts in concepts_of_cognateset.items():
+            central[cognateset] = central_concept(concepts, concept_to_concepticon)
+    else:
+        for cognateset, concepts in concepts_of_cognateset.items():
+            central[cognateset] = central_concept(concepts, {})
+
+    add_central_concepts_to_cognateset_table(
+        dataset,
+        central,
+        add_column=args.add_column,
+        overwrite_existing=args.overwrite_existing,
     )
