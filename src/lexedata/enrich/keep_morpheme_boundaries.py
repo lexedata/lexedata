ca2c1898314e3dc36e070119034e5e231cd07457
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-01-25 12:43:20 +0100

    reshaped keep_morpheme_boundaries.py

--- src/lexedata/enrich/keep_morpheme_boundaries.py
+++ src/lexedata/enrich/keep_morpheme_boundaries.py
@@ -4,6 +4,43 @@ import argparse
 
 import csv
 
+
+def morpheme_boundaries_to_csv(judgements: Path, alignments: Path, output_file: Path):
+    cognatesets_new = {}
+    for line in csv.DictReader(judgements.open()):
+        cognatesets_new.setdefault(line["Cognateset_ID"], set()).add(line["Form_ID"])
+
+    cognatesets_old = {}
+    old_judgements = {}
+    for line in csv.DictReader(alignments.open()):
+        cognatesets_old.setdefault(line["Cognateset_ID"], set()).add(line["Form_ID"])
+        old_judgements[line["Form_ID"], line["Cognateset_ID"]] = line
+
+    cognatesets_old_reversed = {
+        frozenset(forms): id for id, forms in cognatesets_old.items()
+    }
+
+    with judgements.open() as judgements_file:
+        judgements = csv.DictReader(judgements_file)
+        out = csv.DictWriter(
+            output_file.open("w"),
+            judgements.fieldnames,
+            dialect=judgements.dialect,
+        )
+        out.writeheader()
+        for line in judgements:
+            cognates = frozenset(cognatesets_new[line["Cognateset_ID"]])
+            if cognates in cognatesets_old_reversed:
+                old_line = old_judgements[
+                    line["Form_ID"], cognatesets_old_reversed[cognates]
+                ]
+                line["Alignment"] = old_line["Alignment"]
+                line["Segment_Slice"] = old_line["Segment_Slice"]
+            else:
+                pass
+            out.writerow(line)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Transfer metadata of existing cognate sets to new cognatesets that may "
