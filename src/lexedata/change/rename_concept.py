9144782909a18e30befe2217ff09baaa8443b4ff
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-01-25 14:48:46 +0100

    added status update

--- src/lexedata/change/rename_concept.py
+++ src/lexedata/change/rename_concept.py
@@ -1,32 +1,27 @@
 import pycldf
 import argparse
 import typing as t
+from pathlib import Path
 
 from lexedata.enrich.add_status_column import add_status_column_to_table
 
-def substitute(row, columns, old_value, new_value):
-    for column in columns:
-        if type(row[column]) == list:
-            row[column] = [
-                new_value if val == old_value else val for val in row[column]
-            ]
-        elif type(row[column]) == str:
-            row[column] = new_value if row[column] == old_value else row[column]
-    return row
 
-
-def substitute_many(row, columns, old_values_to_new_values):
+def substitute_many(row, columns, old_values_to_new_values, status_update: t.Optional[str]):
     for column in columns:
         if type(row[column]) == list:
             row[column] = [
                 old_values_to_new_values.get(val, val) for val in row[column]
             ]
+            if status_update:
+                row["Status_Column"] = status_update
         elif type(row[column]) == str:
             row[column] = old_values_to_new_values.get(row[column], row[column])
+            if status_update:
+                row["Status_Column"] = status_update
     return row
 
 
-def rename(ds, old_values_to_new_values):
+def rename(ds, old_values_to_new_values, status_update: t.Optional[str]):
     concepts = ds["ParameterTable"]
 
     for table in ds.tables:
@@ -47,11 +42,12 @@ def rename(ds, old_values_to_new_values):
             ds.write(
                 **{
                     component: [
-                        substitute_many(r, columns, old_values_to_new_values)
+                        substitute_many(r, columns, old_values_to_new_values, status_update=status_update)
                         for r in table
                     ]
                 }
             )
+
 
 def replace_column(
         dataset: pycldf.Dataset,
@@ -61,9 +57,11 @@ def replace_column(
         smush: bool,
         status_update: t.Optional[str]
 ) -> None:
-    if column_replace:
-        concepts = dataset["ParameterTable"]
+    # add Status_column if not existing and status update given
+    if status_update:
+        add_status_column_to_table(dataset=dataset, table_name="ParameterTable")
 
+    if column_replace:
         assert (
             original == "id"
             or original == dataset["ParameterTable", "id"].name
@@ -78,7 +76,7 @@ def replace_column(
             set(mapping.values())
         ), "Would collapse some concepts that were distinct before! Add '--smush' if that is intended."
         # dataset["ParameterTable"].tableSchema.columns["c_id"]
-        rename(dataset, mapping)
+        rename(dataset, mapping, status_update=status_update)
     else:
         concepts = dataset["ParameterTable"]
 
@@ -87,11 +85,11 @@ def replace_column(
         print(f"Changing {c_id:} of ParameterTable…")
         dataset.write(
             ParameterTable=[
-                substitute_many(r, [c_id], {original: replacement})
+                substitute_many(r, [c_id], {original: replacement}, status_update=status_update)
                 for r in concepts
             ]
         )
-        rename(dataset, {original: replacement})
+        rename(dataset, {original: replacement}, status_update=None)
 
 
 if __name__ == "__main__":
@@ -124,5 +122,25 @@ if __name__ == "__main__":
         action="store_true",
         default=False
     )
+    parser.add_argument(
+        "--status-update",
+        type=str,
+        default="default",
+        help="Text written to Status_Column. Set to 'None' for no status update. "
+             "(default: Replaced column {original} by column {replacement}",
+    )
+    args = parser.parse_args()
+    if args.status_update == "None":
+        args.status_update = None
+    if args.status_update == "default":
+        args.status_update = f"Replaced column {args.original} by column {args.replacement}"
 
+    replace_column(
+        dataset=pycldf.Dataset.from_metadata(args.metadata),
+        original=args.original,
+        replacement=args.replacement,
+        column_replace=args.column_replace,
+        smush=args.smush,
+        status_update=args.status_update
+    )
 
