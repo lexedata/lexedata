cf7953c337371b060d94cf5ba7cd4d40aee9b6a7
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-01-26 11:00:48 +0100

    status update for importer/cognates.py

--- src/lexedata/importer/cognates.py
+++ src/lexedata/importer/cognates.py
@@ -9,6 +9,7 @@ from lexedata.types import Language, RowObject, CogSet
 import lexedata.importer.cellparser as cell_parsers
 from lexedata.importer.fromexcel import ExcelCognateParser
 from lexedata.util import clean_cell_value, get_cell_comment
+from lexedata.enrich.add_status_column import add_status_column_to_table
 
 
 class CognateEditParser(ExcelCognateParser):
@@ -70,8 +71,6 @@ def header_from_cognate_excel(
             column_name = dataset["CognatesetTable", "id"].name
         try:
             column_name = dataset["CognatesetTable", column_name].name
-        # TODO: @Gereon is this break here intentional. So if one header does not match we stop reading headers?
-        # TODO: Not rather continue?
         except KeyError:
             break
         row_header.append(column_name)
@@ -79,7 +78,7 @@ def header_from_cognate_excel(
     return row_header, separators
 
 
-def import_cognates_from_excel(excel: str, dataset: pycldf.Dataset) -> None:
+def import_cognates_from_excel(excel: str, dataset: pycldf.Dataset, status_update: t.Optional[str]) -> None:
     ws = openpyxl.load_workbook(excel).active
 
     row_header, _ = header_from_cognate_excel(ws, dataset)
@@ -98,11 +97,13 @@ def import_cognates_from_excel(excel: str, dataset: pycldf.Dataset) -> None:
         check_for_match=[dataset["FormTable", "id"].name],
         check_for_row_match=[dataset["CognatesetTable", "id"].name],
     )
-
+    # create status_column if status update is given
+    if status_update:
+        add_status_column_to_table(dataset=dataset, table_name="CognateTable")
     excel_parser_cognate.db.cache_dataset()
     excel_parser_cognate.db.drop_from_cache("CognatesetTable")
     excel_parser_cognate.db.drop_from_cache("CognateTable")
-    excel_parser_cognate.parse_cells(ws)
+    excel_parser_cognate.parse_cells(ws, status_update=status_update)
     excel_parser_cognate.db.write_dataset_from_cache(
         ["CognateTable", "CognatesetTable"]
     )
@@ -125,9 +126,17 @@ if __name__ == "__main__":
         default="Wordlist-metadata.json",
         help="Path to the JSON metadata file describing the dataset (default: ./Wordlist-metadata.json)",
     )
+    parser.add_argument(
+        "--status-update",
+        type=str,
+        default="Imported from cognate excel",
+        help="Text written to Status_Column. Set to 'None' for no status update. "
+             "(default: Imported from cognate excel)",
+    )
 
     args = parser.parse_args()
-
+    if args.status_update == "None":
+        args.status_update = None
     import_cognates_from_excel(
-        args.cogsets, pycldf.Dataset.from_metadata(args.metadata)
+        args.cogsets, pycldf.Dataset.from_metadata(args.metadata), args.status_update
     )
