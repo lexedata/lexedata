11610410a3537e828513a8a0681b2d6ae2536327
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-02-18 22:42:35 +0100

    making the seed run

--- src/lexedata/report/nonconcatenative_morphemes.py
+++ src/lexedata/report/nonconcatenative_morphemes.py
@@ -1,22 +1,45 @@
-# load all cognates corresponding to those forms
-which_segment_belongs_to_which_cognateset: t.Dict[str, t.List[t.Set[str]]] = {}
-for j in dataset["CognateTable"]:
-    if j[c_cognate_form] in forms and j[c_cognate_cognateset] in cognateset_cache:
-        form = forms[j[c_cognate_form]]
-        if j[c_cognate_form] not in which_segment_belongs_to_which_cognateset:
-            which_segment_belongs_to_which_cognateset[j[c_cognate_form]] = [
-                set() for _ in form[c_form_segments]
-            ]
-        segments_judged = segment_slices_to_segment_list(
-            segments=form[c_form_segments], judgement=j
-        )
-        for s in segments_judged:
-            try:
-                which_segment_belongs_to_which_cognateset[j[c_cognate_form]][s].add(
-                    j[c_cognate_cognateset]
-                )
-            except IndexError:
-                print(
-                    f"WARNING: In judgement {j}, segment slice point outside valid range 0:{len(form[c_form_segments])}."
-                )
-                continue
+import typing as t
+
+import pycldf
+
+from lexedata.util import segment_slices_to_segment_list
+
+
+def segment_to_cognateset(dataset: pycldf.Dataset, cognatesets: t.Iterable):
+    # required fields
+    c_cognate_cognateset = dataset.column_names.cognates.cognatesetReference
+    c_form_segments = dataset.column_names.forms.segments
+    c_form_id = dataset.column_names.forms.id
+    c_cognate_form = dataset.column_names.cognates.formReference
+
+    forms = {f[c_form_id]: f for f in dataset["FormTable"]}
+    cognateset_cache: t.Dict[t.Optional[str], int] = {
+        cognateset["ID"]: c
+        for c, cognateset in enumerate(dataset["CognatesetTable"], 1)
+        if cognateset["ID"] in cognatesets
+    }
+    cognateset_cache[None] = 0
+
+    which_segment_belongs_to_which_cognateset: t.Dict[str, t.List[t.Set[str]]] = {}
+    for j in dataset["CognateTable"]:
+        if j[c_cognate_form] in forms and j[c_cognate_cognateset] in cognateset_cache:
+            form = forms[j[c_cognate_form]]
+            if j[c_cognate_form] not in which_segment_belongs_to_which_cognateset:
+                which_segment_belongs_to_which_cognateset[j[c_cognate_form]] = [
+                    set() for _ in form[c_form_segments]
+                ]
+            segments_judged = segment_slices_to_segment_list(
+                segments=form[c_form_segments], judgement=j
+            )
+            for s in segments_judged:
+                try:
+                    which_segment_belongs_to_which_cognateset[j[c_cognate_form]][s].add(
+                        j[c_cognate_cognateset]
+                    )
+                except IndexError:
+                    print(
+                        f"WARNING: In judgement {j}, segment slice point outside valid range 0:{len(form[c_form_segments])}."
+                    )
+                    continue
+    return which_segment_belongs_to_which_cognatese
+
