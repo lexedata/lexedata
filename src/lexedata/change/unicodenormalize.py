faeff290b79b5c7916b025b9a37dc193a1eabaee
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2021-04-08 16:07:05 +0200

    Add another empty skeleton

---
+++ src/lexedata/change/unicodenormalize.py
@@ -0,0 +1,4 @@
+"""Make sure every string entry in every table of the dataset uses NFC unicode normalization.
+
+Maybe this should be one of the extended checks, with automatic fixing option, instead?
+"""
