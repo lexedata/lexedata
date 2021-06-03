5c7eb785cff32d14c3c055a8048812c56b625905
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-06-03 10:36:00 +0200

    black

--- test/test_segment.py
+++ test/test_segment.py
@@ -15,4 +15,4 @@ def test_segment_report():
     report.sounds["aʰ"]["count"] = 1
     report.sounds["aʰ"]["comment"] = "comment"
     report = report("language")
-    assert report==[("language", "aʰ", 1, "comment")]
+    assert report == [("language", "aʰ", 1, "comment")]
