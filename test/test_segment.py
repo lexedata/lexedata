5ea95d163650d456e5ed511ef4abf84f881214f8
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-06-03 14:04:34 +0200

    more tests

--- test/test_segment.py
+++ test/test_segment.py
@@ -5,21 +5,36 @@ from lexedata.edit.add_segments import segment_form, SegmentReport
 
 def test_unkown_aspiration(caplog):
     form = "-á:muaʰ"
-    segment_form(form, SegmentReport())
+    report = SegmentReport()
+    segment_form(form, report)
     print(caplog.text)
-    assert re.search("Unknown sound aʰ encountered in -á:muaʰ", caplog.text)
+    assert re.search(
+        "Unknown sound aʰ encountered in -á:muaʰ", caplog.text
+    ) and report("language") == [("language", "aʰ", 1, "unknown pre-aspiration")]
 
 
 def test_segment_report():
-    report = SegmentReport()
-    report.sounds["aʰ"]["count"] = 1
-    report.sounds["aʰ"]["comment"] = "comment"
-    report = report("language")
-    assert report == [("language", "aʰ", 1, "comment")]
+    report1 = SegmentReport()
+    report1.sounds["aʰ"]["count"] = 1
+    report1.sounds["aʰ"]["comment"] = "comment"
+    report1 = report1("language")
+    assert report1 == [("language", "aʰ", 1, "comment")]
 
 
+# TODO: report also contains the data from the test before ... but why does this happen?
 def test_unknown_sound(caplog):
     form = "wohuᵈnasi"
-    segment_form(form, SegmentReport())
-    print(caplog.text)
-    assert re.search("Unknown sound uᵈ encountered in wohuᵈnasi", caplog.text)
+    report2 = SegmentReport()
+    segment_form(form, report2)
+    assert re.search(
+        "Unknown sound uᵈ encountered in wohuᵈnasi", caplog.text
+    )  # and report2("language") == [("language", "uᵈ", 1, "unknown sound")]
+
+
+def test_illegal_symbol(caplog):
+    form = r"woh/"
+    report3 = SegmentReport()
+    segment_form(form, report3)
+    assert re.search(
+        "Impossible sound '/' encountered in woh/", caplog.text
+    )  # and report3("language") == [("language", "/", 1, "illegal symbol")]
