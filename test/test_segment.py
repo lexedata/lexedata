45084646063a068e8202a85ee80f7d22dfb6a1d6
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-06-03 10:29:17 +0200

    adding a test for segments

--- test/test_segment.py
+++ test/test_segment.py
@@ -8,3 +8,11 @@ def test_unkown_aspiration(caplog):
     segment_form(form, SegmentReport())
     print(caplog.text)
     assert re.search("Unknown sound aʰ encountered in -á:muaʰ", caplog.text)
+
+
+def test_segment_report():
+    report = SegmentReport()
+    report.sounds["aʰ"]["count"] = 1
+    report.sounds["aʰ"]["comment"] = "comment"
+    report = report("language")
+    assert report==[("language", "aʰ", 1, "comment")]
