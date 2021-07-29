7eb8f78510eff59e16bd4eb4a782680f87e0e5e8
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2021-07-29 16:52:20 +0200

    Delete an empty line

--- test/test_clis.py
+++ test/test_clis.py
@@ -9,6 +9,5 @@ def test_exit(caplog):
     with pytest.raises(SystemExit) as exit:
         with caplog.at_level(logging.ERROR):
             cli.Exit.INVALID_DATASET()
-
         assert "INVALID_DATASET" in caplog.text
     assert exit.value.code == 7
