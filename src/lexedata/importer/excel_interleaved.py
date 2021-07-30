bab591a9522defa4d9448782002b7988bd2cdf15
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2021-07-30 13:06:35 +0200

    Add unicode normalization

--- src/lexedata/importer/excel_interleaved.py
+++ src/lexedata/importer/excel_interleaved.py
@@ -16,6 +16,7 @@ from pathlib import Path
 import openpyxl
 
 from lexedata import cli, util, types
+from lexedata.util.excel import clean_cell_value
 
 
 def import_interleaved(
@@ -41,12 +42,12 @@ def import_interleaved(
     for concept_metadata in ws.iter_cols(min_col=1, max_col=1, min_row=2):
         for entry, cogset in zip(concept_metadata[::2], concept_metadata[1::2]):
             try:
-                concepts.append(entry.value.strip())
+                concepts.append(clean_cell_value(entry))
             except AttributeError:
                 break
 
     for language in ws.iter_cols(min_col=2):
-        language_name = language[0].value
+        language_name = clean_cell_value(language[0])
         for c, (entry, cogset) in enumerate(zip(language[1::2], language[2::2])):
             if not entry.value:
                 if cogset.value:
@@ -56,7 +57,7 @@ def import_interleaved(
                 continue
             bracket_level = 0
             i = 0
-            f = entry.value.strip()
+            f = clean_cell_value(entry)
             forms = []
             while i < len(f):
                 match = comma_or_semicolon.match(f[i:])
@@ -81,13 +82,10 @@ def import_interleaved(
 
             forms.append(f.strip())
 
-            if type(cogset.value) == float:
-                cogsets = [str(int(cogset.value))]
+            if isinstance(clean_cell_value(cogset), int):
+                cogsets = [str(clean_cell_value(cogset))]
             else:
-                if isinstance(cogset.value, int):
-                    cogset = str(cogset.value)
-                else:
-                    cogset = cogset.value
+                cogset = clean_cell_value(cogset)
                 cogsets = comma_or_semicolon.split(cogset.strip())
 
             if len(cogsets) == 1 or len(cogsets) == len(forms):
