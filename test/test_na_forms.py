fd47aed0ec9e06e032ad6910312409091c9770b6
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-09-11 16:47:19 +0200

    adding tests for the importers

--- test/test_na_forms.py
+++ test/test_na_forms.py
@@ -1,3 +1,20 @@
+from pathlib import Path
+import tempfile
+from collections import OrderedDict
+
+import pytest
+import pycldf
+import openpyxl as op
+
+from lexedata.exporter.edictor import forms_to_tsv
+from lexedata.importer.excel_matrix import load_dataset
+from lexedata.importer.excel_interleaved import import_interleaved
+from lexedata.importer.excel_long_format import add_single_languages
+from lexedata.types import WorldSet
+from lexedata.util.fs import copy_dataset
+from helper_functions import copy_metadata
+import lexedata.cli as cli
+
 """Test Lexedata handling of NA values
 
 In https://github.com/cldf/cldf/issues/108, we started a discussion concerning
@@ -29,16 +46,106 @@ tested by this module. It affects multiple different components of Lexedata.
 
 """
 
-
-def test_import_skips_question_marks():
+@pytest.mark.skip()
+def test_matrix_import_skips_question_marks():
     data = [  # noqa
         ["Concept", "L1", "L2"],
         ["C1", "?", "Form1"],
         ["C2", "Form2", "Form3"],
     ]
+    # create excel with data
+    wb = op.Workbook()
+    ws = wb.active
+    for row in data:
+        ws.append(row)
+    dirname = Path(tempfile.mkdtemp(prefix="lexedata-test"))
+    target = dirname / "test.xlsx"
+    wb.save(target)
+
+    # TODo: struggeling to make a matrix importer run on such a simple structure in a way whitout too much coding ... this might even be a bad sign?
+    # create simple metadata
+    dataset = pycldf.Wordlist.in_dir(dirname)
+    dataset.write(FormTable=[  # noqa
+        {"Concept": "", "L1": "", "L2": "", "value": ""}
+         ]
+                  )
+    #metadata = dataset.tablegroup._fname
+    metadata = Path(__file__).parent / "data\cldf\minimal\cldf-metadata.json"
+    # load the dataset
+    load_dataset(
+        metadata=metadata,
+        lexicon=target,
+    )
+    dataset = pycldf.Dataset.from_metadata(metadata)
+    forms = {f for f in dataset["FormTable"]}
+    print(forms)
+    assert True
     # TODO: Put data into an Excel sheet
     # TODO: Run a matrix importer
     # TODO: Check that the resulting dataset has no entry for C1 in L1
+
+
+def test_interleaved_import_skips_na():
+    data = [
+        ["", "Duala", "Ntomba"],
+        ["all", "ɓɛ́sɛ̃(nk)", "umá"],
+        ["", "1", "?"],
+        ["arm", "?", "lobɔ́kɔ"],
+        ["", "7", "1"]
+    ]
+
+    # create excel with data
+    wb = op.Workbook()
+    ws = wb.active
+    for row in data:
+        ws.append(row)
+    # import excel
+    forms = [r for r in import_interleaved(ws)]
+
+    assert forms == [
+        ['duala_all', 'Duala', 'all', 'ɓɛ́sɛ̃(nk)', None, '1'],
+        ['ntomba_arm', 'Ntomba', 'arm', 'lobɔ́kɔ', None, '1']
+    ]
+
+
+def test_single_excel_import_skips_na():
+    data = [ # noqa
+        ["phonetic", "Form", "English"],
+        ["aa", "e.ta.'kɾã", "one"],
+        ["bb", "mĩ.'ɾõ1", "two"],
+        ["?", "?", "one"],
+    ]
+    # create excel with data
+    wb = op.Workbook()
+    ws = wb.active
+    for row in data:
+        ws.append(row)
+    sheets = [sheet for sheet in wb]
+    metadata = copy_metadata(
+        Path(__file__).parent / "data\cldf\minimal\cldf-metadata.json"
+    )
+    #metadata = metadata.
+    _ = add_single_languages(
+        metadata=metadata,
+        sheets=sheets,
+        match_form=[],
+        concept_name="English",
+        ignore_superfluous=True,
+        ignore_missing=True,
+        status_update=None,
+        logger=cli.logger
+    )
+    dataset = pycldf.Dataset.from_metadata(metadata)
+    forms = [f for f in dataset["FormTable"]]
+    assert forms == [
+        OrderedDict(
+            [('ID', 'sheet_one'), ('Language_ID', 'Sheet'), ('Concept_ID', 'one'), ('Form', "e.ta.'kɾã"),
+             ('Segments', []), ('Value', "aa\te.ta.'kɾã\tone"), ('Comment', None), ('Source', [])]),
+        OrderedDict(
+            [('ID', 'sheet_two'), ('Language_ID', 'Sheet'), ('Concept_ID', 'two'), ('Form', "mĩ.'ɾõ1"),
+             ('Segments', []), ('Value', "bb\tmĩ.'ɾõ1\ttwo"), ('Comment', None), ('Source', [])])
+    ]
+
 
 
 def test_import_loads_dash():
@@ -88,25 +195,51 @@ def test_phylogenetics_exporter_unknown():
 
 def test_edictor_no_na_forms():
     forms = [  # noqa
-        {"ID": "L1C1", "Language_ID": "L1", "Concept_ID": "C1", "Form": ""},
-        {"ID": "L2C1", "Language_ID": "L2", "Concept_ID": "C1", "Form": "L2C1"},
-        {"ID": "L1C2", "Language_ID": "L1", "Concept_ID": "C2", "Form": "L1C2"},
-        {"ID": "L2C2", "Language_ID": "L2", "Concept_ID": "C2", "Form": "-"},
+        {"ID": "L1C1", "Language_ID": "L1", "Parameter_ID": "C1", "Form": "", "Value": " "},
+        {"ID": "L2C1", "Language_ID": "L2", "Parameter_ID": "C1", "Form": "L2C1", "Value": " "},
+        {"ID": "L1C2", "Language_ID": "L1", "Parameter_ID": "C2", "Form": "L1C2", "Value": " "},
+        {"ID": "L2C2", "Language_ID": "L2", "Parameter_ID": "C2", "Form": "-", "Value": " "},
     ]
     cognates = [  # noqa
         {"Form_ID": "L2C1", "Cognateset_ID": "1"},
         {"Form_ID": "L1C2", "Cognateset_ID": "2"},
     ]
+    dirname = Path(tempfile.mkdtemp(prefix="lexedata-test"))
+    target = dirname / "cldf-metadata.json"
+    dataset = copy_dataset(
+            original=Path(__file__).parent / "data/cldf/smallmawetiguarani/cldf-metadata.json",
+            target=target
+        )
+
+    dataset.write(FormTable=forms)
+    forms, judgements_about_form, cognateset_mapping = forms_to_tsv(
+        dataset=dataset,
+        languages=WorldSet(),
+        concepts=WorldSet(),
+        cognatesets=cognates
+                 )
+    print(forms)
+    assert forms == {
+        'L2C1': OrderedDict([('ID', 'L2C1'), ('Language_ID', 'L2'), ('Parameter_ID', 'C;1'), ('Form', 'L2C1'),
+                             ('orthographic', None), ('phonemic', None), ('phonetic', None), ('variants', ''),
+                             ('Segments', []), ('Comment', None), ('procedural_comment', None),
+                             ('Value', ' '), ('Source', '')]),
+        'L1C2': OrderedDict([('ID', 'L1C2'), ('Language_ID', 'L1'), ('Parameter_ID', 'C;2'), ('Form', 'L1C2'),
+                             ('orthographic', None), ('phonemic', None), ('phonetic', None), ('variants', ''),
+                             ('Segments', []), ('Comment', None), ('procedural_comment', None), ('Value', ' '),
+                             ('Source', '')])
+    }
+
     # TODO: Run the edictor exporter
     # TODO: Check that the edictor export contains only L2C1 and L1C2
 
 
 def test_detect_cognates_ignores_na_forms():
     forms = [  # noqa
-        {"ID": "L1C1", "Language_ID": "L1", "Concept_ID": "C1", "Form": ""},
-        {"ID": "L2C1", "Language_ID": "L2", "Concept_ID": "C1", "Form": "L2C1"},
-        {"ID": "L1C2", "Language_ID": "L1", "Concept_ID": "C2", "Form": "L1C2"},
-        {"ID": "L2C2", "Language_ID": "L2", "Concept_ID": "C2", "Form": "-"},
+        {"ID": "L1C1", "Language_ID": "L1", "Parameter_ID": "C1", "Form": ""},
+        {"ID": "L2C1", "Language_ID": "L2", "Parameter_ID": "C1", "Form": "L2C1"},
+        {"ID": "L1C2", "Language_ID": "L1", "Parameter_ID": "C2", "Form": "L1C2"},
+        {"ID": "L2C2", "Language_ID": "L2", "Parameter_ID": "C2", "Form": "-"},
     ]
     # TODO: run ACD
     # TODO: Check that the cognatesets contain only L2C1 and L1C2
