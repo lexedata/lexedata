301df4973a7effbf456f298a9ec9f921f741d072
Gereon Kaiping <gereon.kaiping@gmail.com>
2021-09-23 20:52:24 +0200

    Add first stub of homophone merger

---
+++ src/lexedata/edit/merge_homophones.py
@@ -0,0 +1,9 @@
+"""Read a homophones report (an edited one, most likely) and merge all pairs of form in there.
+
+Different treatment for separated fields, and un-separated fields
+Form variants into variants?
+Make sure concepts have a separator
+What other columns give warnings, what other columns give errors?
+
+*Optionally*, merge cognate sets that get merged by this procedure.
+"""
