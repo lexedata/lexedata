8bcb2c4d7e030dd7db7925842c5e8aba96016f77
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-09-24 17:57:02 +0200

    fixed some bugs

--- src/lexedata/edit/add_singleton_cognatesets.py
+++ src/lexedata/edit/add_singleton_cognatesets.py
@@ -26,7 +26,7 @@ def create_singeltons(dataset: pycldf.Dataset, logger: cli.logger = cli.logger):
     c_c_id = dataset["CognateTable", "id"].name
 
     status_column = None
-    for column in dataset["CognateTable"].tableSchema.columns:
+    for column in dataset["CognatesetTable"].tableSchema.columns:
         if column.name == "Status_Column":
             status_column = column.name
     if not status_column:
@@ -40,10 +40,6 @@ def create_singeltons(dataset: pycldf.Dataset, logger: cli.logger = cli.logger):
     for foreign_key in dataset["FormTable"].tableSchema.foreignKeys:
         if foreign_key.reference.resource == dataset["ParameterTable"].url:
             foreign_key_form_concept = foreign_key.columnReference[0]
-    foreign_key_form_cognate = ""
-    for foreign_key in dataset["FormTable"].tableSchema.foreignKeys:
-        if foreign_key.reference.resource == dataset["CognateTable"].url:
-            foreign_key_form_cognate = foreign_key.columnReference[0]
 
     foreign_key_form_language = ""
     for foreign_key in dataset["FormTable"].tableSchema.foreignKeys:
@@ -109,29 +105,14 @@ def create_singeltons(dataset: pycldf.Dataset, logger: cli.logger = cli.logger):
             foreign_key_cognate_cogset: cogset[c_cs_id],
         }
         judgements.append(cognate)
-        this_form = all_forms[f[c_f_id]]
-        this_form[foreign_key_form_cognate] = cognate[c_c_id]
-        all_forms[f[c_f_id]] = this_form
-    new_forms = []
-    for f in all_forms.values():
-        new_forms.append(f)
-    return new_forms, all_cogsets, judgements
-
-
-def write_singletons_to_dataset(dataset: pycldf.Dataset, logger: cli.logger):
-    logger.info("Creating Singleton Cognatesets.")
-    all_forms, all_cogsets, judgements = create_singeltons(
-        dataset=dataset, logger=logger
-    )
-    dataset.write(
-        FormTable=all_forms, CognatesetTable=all_cogsets, CognateTable=judgements
-    )
+    return all_cogsets, judgements
 
 
 if __name__ == "__main__":
     parser = cli.parser(description="Create an Excel cognate view from a CLDF dataset")
     args = parser.parse_args()
     logger = cli.setup_logging(args)
-    write_singletons_to_dataset(
-        dataset=pycldf.Dataset.from_metadata(args.metadata), logger=logger
-    )
+    logger.info("Creating Singleton Cognatesets.")
+    dataset = pycldf.Dataset.from_metadata(args.metadata)
+    all_cogsets, judgements = create_singeltons(dataset=dataset, logger=logger)
+    dataset.write(CognatesetTable=all_cogsets, CognateTable=judgements)
