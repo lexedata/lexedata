fd2144bba298e14fe67486387785aaf1ba531e53
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-09-24 14:45:02 +0200

    singletons complete

--- src/lexedata/edit/add_singleton_cognatesets.py
+++ src/lexedata/edit/add_singleton_cognatesets.py
@@ -10,36 +10,65 @@ from collections import OrderedDict
 
 import pycldf
 
+from lexedata import types
+from lexedata import cli
 # Type aliases, for clarity
 CognatesetID = str
 FormID = str
+ConceptID = str
+
 
 def create_singeltons(dataset: pycldf.Dataset):
     # cldf names and foreignkeys
-    c_f_id = dataset["FormTable", "id"]
+    c_f_id = dataset["FormTable", "id"].name
     c_cs_id = dataset["CognatesetTable", "id"].name
     c_cs_name = dataset["CognatesetTable", "name"].name
-    foreign_key_form_cogset = ""
+    c_c_id = dataset["CognateTable", "id"].name
+
+    foreign_key_form_concept = ""
     for foreign_key in dataset["FormTable"].tableSchema.foreignKeys:
-        if foreign_key.reference.resource == dataset["CognatesetTable"].url:
-            foreign_key_form_cogset = foreign_key.columnReference[0]
+        if foreign_key.reference.resource == dataset["ParameterTable"].url:
+            foreign_key_form_concept = foreign_key.columnReference[0]
+    foreign_key_form_cognate = ""
+    for foreign_key in dataset["FormTable"].tableSchema.foreignKeys:
+        if foreign_key.reference.resource == dataset["CognateTable"].url:
+            foreign_key_form_cognate = foreign_key.columnReference[0]
+
+    foreign_key_form_language = ""
+    for foreign_key in dataset["FormTable"].tableSchema.foreignKeys:
+        if foreign_key.reference.resource == dataset["LanguageTable"].url:
+            foreign_key_form_language = foreign_key.columnReference[0]
 
     foreign_key_cognate_form = ""
     for foreign_key in dataset["CognateTable"].tableSchema.foreignKeys:
         if foreign_key.reference.resource == dataset["FormTable"].url:
             foreign_key_cognate_form = foreign_key.columnReference[0]
+
     foreign_key_cognate_cogset = ""
     for foreign_key in dataset["CognateTable"].tableSchema.foreignKeys:
         if foreign_key.reference.resource == dataset["CognatesetTable"].url:
             foreign_key_cognate_cogset = foreign_key.columnReference[0]
+
     # load data
     singleton_forms: t.Dict[FormID, OrderedDict] = {}
     for f in dataset["FormTable"]:
         singleton_forms[f[c_f_id]] = f
+
     all_judgements: t.Dict[CognatesetID, t.List[types.CogSet]] = {}
     for j in dataset["CognateTable"]:
         all_judgements.setdefault(j[foreign_key_cognate_cogset], []).append(j)
+
     all_cogsets = [c for c in dataset["CognatesetTable"]]
+
+    concept_id_by_form_id: t.Dict[ConceptID, FormID] = {}
+    for f in dataset["FormTable"]:
+        concept = f[foreign_key_form_concept]
+        if isinstance(concept, str):
+            concept_id_by_form_id[f[c_f_id]] = concept
+        else:
+            concept_id_by_form_id[f[c_f_id]] = concept[0]
+
+    # delete forms with cognateset
     for k in all_judgements:
         for j in all_judgements[k]:
             form_id = j[foreign_key_cognate_form]
@@ -47,15 +76,43 @@ def create_singeltons(dataset: pycldf.Dataset):
                 del singleton_forms[form_id]
             except KeyError:
                 continue
-    # create singletons for remaining forms and add singleton to cogsets
-    for i, form_id in enumerate(singleton_forms):
-        cogset_id =
-        if db_name == c_cogset_id:
-            value = f"X{i + 1}_{form[c_language]}"
-        elif db_name == c_cogset_name:
-            value = concept_id_by_form_id[form_id]
-        elif db_name == c_cogset_concept:
-            value = concept_id_by_form_id[form_id]
-    # replace forms with new singleton cogset id
+    del all_judgements
+    judgements = [c for c in dataset["CognateTable"]]
+    # create singletons for remaining forms and add singleton to cogsets and replace corresponding forms
+    all_forms: t.Dict[FormID, types.Form] = {}
+    for f in dataset["FormTable"]:
+        all_forms[f[c_f_id]] = f
 
-def
+    for i, form_id in enumerate(singleton_forms):
+        cogset = {
+            c_cs_id: f"X{i + 1}_{singleton_forms[form_id][foreign_key_form_language]}",
+            c_cs_name: concept_id_by_form_id[form_id]
+        }
+        all_cogsets.append(cogset)
+
+        cognate = {
+            c_c_id: f"{cogset[c_cs_id]}",
+            foreign_key_cognate_form: form_id,
+            foreign_key_cognate_cogset: cogset[c_cs_id]
+        }
+        judgements.append(cognate)
+        this_form = all_forms[f[c_f_id]]
+        this_form[foreign_key_form_cognate] = cognate[c_c_id]
+        all_forms[f[c_f_id]] = this_form
+    new_forms = []
+    for f in all_forms.values():
+        new_forms.append(f)
+    return new_forms, all_cogsets, judgements
+
+
+def write_singletons_to_dataset(dataset: pycldf.Dataset):
+    all_forms, all_cogsets, judgements = create_singeltons(dataset=dataset)
+    dataset.write(FormTable=all_forms, CognatesetTable=all_cogsets, CognateTable=judgements)
+
+
+if __name__ == "__main__":
+    parser = cli.parser(description="Create an Excel cognate view from a CLDF dataset")
+    args = parser.parse_args()
+    # logger = cli.setup_logging(args)
+    write_singletons_to_dataset(dataset=pycldf.Dataset.from_metadata(args.metadata))
+
