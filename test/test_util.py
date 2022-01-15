83b1174623de899451f1d080ef1218c4d812030e
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2022-01-15 20:41:37 +0100

    Revert "function get foreign_key"
    This reverts commit fd1dcd7cac455369b32f600485121b67ef5de6fc.

--- test/test_util.py
+++ test/test_util.py
@@ -4,8 +4,7 @@ from pathlib import Path
 
 import pycldf
 
-from lexedata.util import normalize_table_name, get_foreignkey
-from helper_functions import copy_metadata
+from lexedata.util import normalize_table_name
 
 
 @pytest.fixture(params=["data/cldf/smallmawetiguarani/cldf-metadata.json"])
@@ -22,21 +21,3 @@ def test_normal_table_names(wordlist, caplog):
     with caplog.at_level(logging.WARNING):
         assert normalize_table_name("NonExistingTable", wordlist) is None
     assert "Could not find table NonExistingTable" in caplog.text
-
-
-def test_get_foreignkey():
-    dataset = pycldf.Dataset.from_metadata(
-        copy_metadata(
-            original=Path(__file__).parent / "data/cldf/smallmawetiguarani/cldf-metadata.json"
-        )
-    )
-    foreign_key_parameter = ""
-    for foreign_key in dataset["FormTable"].tableSchema.foreignKeys:
-        if foreign_key.reference.resource == dataset["ParameterTable"].url:
-            foreign_key_parameter = foreign_key.columnReference[0]
-    assert foreign_key_parameter == get_foreignkey(
-        dataset=dataset,
-        table="FormTable",
-        other_table="ParameterTable",
-    )
-
