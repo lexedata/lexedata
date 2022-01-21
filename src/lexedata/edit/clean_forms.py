lexedata/lexedata

commit 5fc83bb9d43b41c4c1f616426d37ab34a538d8cb

fix doctests

src/lexedata/edit/clean_forms.py
@@ -97,6 +97,7 @@ def treat_brackets(
     {'F': 'da', 'V': [], 'C': '(dialectal)'}
     {'F': 'tum', 'V': ['tu'], 'C': '2p; (informal)'}
 
+
     Skipping works even when it is noticed only late in the process.
 
     >>> for row in treat_brackets([
@@ -104,8 +105,9 @@ def treat_brackets(
     ...   {'F': 'tu(m) (informal', 'V': [], 'C': ''}],
     ...   "F", "V", "C", [("[", "]"), ("(", ")")]):
     ...   print(row)
-    {'F': 'a[m]ba (unbalanced', 'V': [], 'C': ''}
-    {'F': 'tu(m) (informal', 'V': [], 'C': ''}
+    {'F': 'amba', 'V': ['aba'], 'C': ''}
+    {'F': 'da', 'V': [], 'C': '(dialectal)'}
+    {'F': 'tum', 'V': ['tu'], 'C': '2p; (informal)'}
 
     """
     for r, row in enumerate(table):
@@ -123,7 +125,7 @@ def treat_brackets(
                 variants.extend(new_variants)
                 comment.extend(new_comments)
             # TODO: Should we have a message here?
-            yield row | {
+            yield row or {
                 form_column_name: form,
                 variants_column_name: variants,
                 comment_column_name: "; ".join(comment),

src/lexedata/exporter/phylogenetics.py
@@ -371,7 +371,7 @@ def apply_heuristics(
     >>> ds.write(CognatesetTable=[
     ...     {"ID": "cognateset1", "Central_Concept": "concept1"}
     ... ])
-    >>> apply_heuristics(ds, heuristic=AbsenceHeuristic.CentralConcept) == {'cognateset1': {'concept1'}}
+    >>> apply_heuristics(ds, heuristic=AbsenceHeuristic.CENTRALCONCEPT) == {'cognateset1': {'concept1'}}
     True
 
     This extends to the case where a cognateset may have more than one central concept.
@@ -387,7 +387,7 @@ def apply_heuristics(
     >>> ds.write(CognatesetTable=[
     ...     {"ID": "cognateset1", "Central_Concepts": ["concept1", "concept2"]}
     ... ])
-    >>> apply_heuristics(ds, heuristic=AbsenceHeuristic.CentralConcept) == {
+    >>> apply_heuristics(ds, heuristic=AbsenceHeuristic.CENTRALCONCEPT) == {
     ...     'cognateset1': {'concept1', 'concept2'}}
     True
 
@@ -401,7 +401,7 @@ def apply_heuristics(
     ...     CognateTable=[
     ...         {"ID": "1", "Form_ID": "f1", "Cognateset_ID": "s1"},
     ...         {"ID": "2", "Form_ID": "f2", "Cognateset_ID": "s1"}])
-    >>> apply_heuristics(ds, heuristic=AbsenceHeuristic.HalfPrimaryConcepts) == {
+    >>> apply_heuristics(ds, heuristic=AbsenceHeuristic.HALFPRIMARYCONCEPTS) == {
     ...     's1': {'c1', 'c2'}}
     True
 
@@ -414,9 +414,9 @@ def apply_heuristics(
         heuristic
         if heuristic is not None
         else (
-            AbsenceHeuristic.CentralConcept
+            AbsenceHeuristic.CENTRALCONCEPT
             if ("CognatesetTable", "parameterReference") in dataset
-            else AbsenceHeuristic.HalfPrimaryConcepts
+            else AbsenceHeuristic.HALFPRIMARYCONCEPTS
         )
     )
 
@@ -424,7 +424,7 @@ def apply_heuristics(
         types.Cognateset_ID, t.Set[types.Parameter_ID]
     ] = t.DefaultDict(set)
 
-    if heuristic is AbsenceHeuristic.HalfPrimaryConcepts:
+    if heuristic is AbsenceHeuristic.HALFPRIMARYCONCEPTS:
         c_f = dataset["CognateTable", "formReference"].name
         c_s = dataset["CognateTable", "cognatesetReference"].name
         concepts = util.cache_table(
@@ -437,7 +437,7 @@ def apply_heuristics(
             for concept in util.ensure_list(form["concepts"]):
                 relevant_concepts[j[c_s]].add(concept)
 
-    elif heuristic is AbsenceHeuristic.CentralConcept:
+    elif heuristic is AbsenceHeuristic.CENTRALCONCEPT:
         c_cognateset_concept = dataset["CognatesetTable", "parameterReference"].name
         c_id = dataset["CognatesetTable", "id"].name
         for c in dataset["CognatesetTable"]:

commit 04176ef8683c6530ee349bfa0526c3ee6e1cc7d5

Fix issue in clean_forms
Issue was introduced in 5fc83bb9d43b41c4c1f616426d37ab34a538d8cb

Also move the tour's test down, because when it fails, it's less informative
than the other tests. So a doctest or a unittest failing should happen before we
run our rough pass through the entire package's CLI

src/lexedata/edit/clean_forms.py
@@ -105,9 +105,8 @@ def treat_brackets(
     ...   {'F': 'tu(m) (informal', 'V': [], 'C': ''}],
     ...   "F", "V", "C", [("[", "]"), ("(", ")")]):
     ...   print(row)
-    {'F': 'amba', 'V': ['aba'], 'C': ''}
-    {'F': 'da', 'V': [], 'C': '(dialectal)'}
-    {'F': 'tum', 'V': ['tu'], 'C': '2p; (informal)'}
+    {'F': 'a[m]ba (unbalanced', 'V': [], 'C': ''}
+    {'F': 'tu(m) (informal', 'V': [], 'C': ''}
 
     """
     for r, row in enumerate(table):
@@ -125,7 +124,7 @@ def treat_brackets(
                 variants.extend(new_variants)
                 comment.extend(new_comments)
             # TODO: Should we have a message here?
-            yield row or {
+            yield row | {
                 form_column_name: form,
                 variants_column_name: variants,
                 comment_column_name: "; ".join(comment),

tox.ini
@@ -31,8 +31,8 @@ commands =
   flake8 src/ test/ setup.py
   # mypy --ignore-missing-imports src/
   # mypy src/
-  python -m scriptdoctest -oFAIL_FAST -oREPORT_UDIFF -oCOVERAGE docs/tour.rst
   pytest --doctest-modules --cov-append --cov=lexedata {envsitepackagesdir}/lexedata
   pytest --cov=lexedata --cov-append test/
+  python -m scriptdoctest -oFAIL_FAST -oREPORT_UDIFF -oCOVERAGE docs/tour.rst
   python test/check_all_versions_match.py
   codecov