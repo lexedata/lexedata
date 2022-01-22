9a61394f307f81bbf58b56f3f885414ec24aede0
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2022-01-22 22:46:55 +0100

    No cover for misused ChangeLogLevel action

--- src/lexedata/cli.py
+++ src/lexedata/cli.py
@@ -44,7 +44,7 @@ def tq(iter, task, logger=logger, total: t.Optional[t.Union[int, float]] = None)
 
 class ChangeLoglevel(argparse.Action):
     def __init__(self, option_strings, dest, const, nargs=None, **kwargs):
-        if nargs is not None:
+        if nargs is not None:  # pragma: no cover
             raise ValueError("nargs not allowed")
         self.change = const
         super().__init__(option_strings, dest, nargs=0, **kwargs)
