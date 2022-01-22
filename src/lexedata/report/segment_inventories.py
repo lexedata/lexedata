8fedf7a16a8565b2e22144d4b4cf6aaea2cc12c3
Gereon A. Kaiping <gkaiping@geo.uzh.ch>
2022-01-22 13:17:02 +0100

    Implement segment inventories report
    For #265

--- src/lexedata/report/segment_inventories.py
+++ src/lexedata/report/segment_inventories.py
@@ -1,1 +1,105 @@
-"""Check that the segments are valid CLTS."""
+"""Report the segment inventory of each language.
+
+
+Report the phonemes (or whatever is represented by the #segments column) for
+each language, each with frequencies and whether the segments are valid CLTS.
+"""
+
+import typing as t
+
+import pycldf
+
+from tabulate import tabulate
+from lexedata import cli, types
+from lexedata.edit.add_segments import bipa
+
+
+def count_segments(
+    dataset: types.Wordlist[
+        types.Language_ID,
+        types.Form_ID,
+        types.Parameter_ID,
+        types.Cognate_ID,
+        types.Cognateset_ID,
+    ],
+    languages: t.Container[types.Language_ID],
+):
+    c_f_language = dataset["FormTable", "languageReference"].name
+    try:
+        c_f_segments = dataset["FormTable", "segments"].name
+    except KeyError:
+        cli.Exit.NO_SEGMENTS(
+            """Segment invertories report requires your dataset to have segments in the FormTable.
+        Run `lexedata.edit.add_segments` to automatically add segments based on your forms."""
+        )
+    counter: t.MutableMapping[t.Counter[str]] = t.DefaultDict(t.Counter)
+    for form in cli.tq(
+        dataset["FormTable"],
+        total=dataset["FormTable"].common_props.get("dc:extent"),
+        task="Reading all forms",
+    ):
+        if form[c_f_language] in languages:
+            counter[form[c_f_language]].update(form[c_f_segments])
+    return counter
+
+
+def comment_on_sound(sound: str) -> str:
+    if bipa[sound].type in {"vowel", "consonant", "tone"}:
+        return ""
+    if bipa[sound].type in {"marker"}:
+        return "Marker"
+    return "Invalid BIPA"
+
+
+if __name__ == "__main__":
+    parser = cli.parser(
+        description=__doc__.split("\n\n\n")[0], epilog=__doc__.split("\n\n\n")[1]
+    )
+    parser.add_argument(
+        "--language",
+        action="append",
+        default=[],
+        help="Restrict the report to these lanugage id(s). (default: All languages.)",
+    )
+    args = parser.parse_args()
+    logger = cli.setup_logging(args)
+
+    dataset = pycldf.Wordlist.from_metadata(args.metadata)
+
+    if not args.language:
+        args.language = types.WorldSet()
+
+    counts = count_segments(dataset, args.language)
+
+    if len(counts) == 1:
+        # A single language
+        ((language, scounts),) = counts.items()
+        print(language)
+        print(
+            tabulate(
+                (
+                    (sound, frequency, comment_on_sound(sound))
+                    for sound, frequency in scounts.most_common()
+                ),
+                headers=["Sound", "Occurrences", "Comment"],
+                tablefmt="orgtbl",
+            )
+        )
+    else:
+        # Extended report
+        print(
+            tabulate(
+                (
+                    (
+                        language_id if s == 0 else "",
+                        sound,
+                        frequency,
+                        comment_on_sound(sound),
+                    )
+                    for language_id, scounts in sorted(counts.items())
+                    for s, (sound, frequency) in enumerate(scounts.most_common())
+                ),
+                headers=["Sound", "Occurrences", "Comment"],
+                tablefmt="orgtbl",
+            )
+        )
