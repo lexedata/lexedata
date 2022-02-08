7d6feed478ad9fd1681c28b272ebef2cc1319dce
Gereon Kaiping <gereon.kaiping@gmail.com>
2022-02-08 11:27:23 +0100

    Fix typo
    Pointed out in https://github.com/Anaphory/lexedata/pull/299#discussion_r800933787

--- src/lexedata/edit/clean_forms.py
+++ src/lexedata/edit/clean_forms.py
@@ -124,7 +124,7 @@ def treat_brackets(
                 variants.extend(new_variants)
                 comment.extend(new_comments)
             # We avoid dict.update() here, so that in a recursive call where an
-            # earier bracket has already succeeded, still the whole form cell
+            # earlier bracket has already succeeded, still the whole form cell
             # is skipped. Or at least I thought that was the logic, except
             # there are no recursive calls to treat_brackets.
             yield {
